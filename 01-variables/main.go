d331c0085ec8392c0b1a3194659c95ff4ab45758
Sunil Rajput <>
2023-08-16 14:56:59 -0500

    local and global variables.

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+/* global variable declaration */
+var a int = 20
+
 func main() {
 	// Static Type Declaration
 	// Define variable and then assign value to variable
@@ -17,12 +20,6 @@ func main() {
 	fmt.Printf(" Name : %s %s \n", fname, lname)
 	fmt.Println(" Age is : ", age)
 
-	/*
-		You can also define more than one variable at the same time.
-		var  c, ch byte;
-		var  f, salary float32;
-	*/
-
 	// Constants - variables.
 	const LENGTH int = 10
 	const WIDTH int = 5
@@ -31,4 +28,29 @@ func main() {
 	area = LENGTH * WIDTH
 	fmt.Println("value of area : ", area)
 
+	/* local variable declaration in main function */
+	var a, b, c int
+	a = 10
+	b = 20
+	c = 0
+
+	fmt.Printf("value of a in main() = %d\n", a)
+	c = sum(a, b)
+	fmt.Printf("value of c in main() = %d\n", c)
+
 }
+
+/* function to add two integers */
+func sum(a, b int) int {
+	fmt.Printf("value of a in sum() = %d\n", a)
+	fmt.Printf("value of b in sum() = %d\n", b)
+
+	return a + b
+}
+
+/* output of scope variables.
+value of a in main() = 10
+value of a in sum() = 10
+value of b in sum() = 20
+value of c in main() = 30
+*/
