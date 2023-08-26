f7898408b3b49ea65be7994b457bae7e01b90204
sunil Rajput <sunilrajput@sunils-MBP.lan>
2023-08-26 17:20:26 -0500

    code completed.

--- 09-goroutine/hello-world/main.go
+++ 09-goroutine/hello-world/main.go
@@ -5,26 +5,33 @@ import (
 	"time"
 )
 
-func fun(s string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(s)
-		time.Sleep(1 * time.Millisecond)
-	}
-}
+func printString(s string) {
+   for i:= 0; i< 3; i++ {
+	fmt.Println(s)
+	time.Sleep(1 * time.Millisecond)
+   }
+ }
 
 func main() {
 	// Direct call
-	fun("direct call")
+	printString("Direct call ")
 
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	   go printString("called by goroutine-1 ")
 
 	// goroutine with anonymous function
+	go func() {
+		printString("called by goroutine-2")
+	}() 
 
 	// goroutine with function value call
+	fv := printString 
+	go fv("called by goroutine-3")  	
 
 	// wait for goroutines to end
+	time.Sleep( 100 * time.Millisecond )
 
-	fmt.Println("done..")
+	fmt.Println("")
 }
