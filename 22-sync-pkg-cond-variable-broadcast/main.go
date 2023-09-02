bfe944a2be969f07af46591bef9debe527f21090
sunil Rajput <sunilrajput@sunils-MBP.lan>
2023-09-01 20:39:16 -0500

    sync pakcage conditional variable broadcast method implementation.

---
+++ 22-sync-pkg-cond-variable-broadcast/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var sharedRsc = make(map[string]interface{})
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	m := sync.Mutex{}
+	c := sync.NewCond(&m)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		c.L.Lock()
+		for len(sharedRsc) == 0 {
+			c.Wait()
+		}
+		fmt.Println(sharedRsc["rsc1"])
+		c.L.Unlock()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		c.L.Lock()
+
+		for len(sharedRsc) == 0 {
+			c.Wait()
+		}
+		fmt.Println(sharedRsc["rsc2"])
+
+		c.L.Unlock()
+	}()
+
+	fmt.Println("main goroutine ready..")
+
+	// TODO: writes changes to sharedRsc
+	c.L.Lock()
+	sharedRsc["rsc1"] = "foo"
+	sharedRsc["rsc2"] = "bar"
+	c.Broadcast()
+
+	c.L.Unlock()
+
+	wg.Wait()
+
+}
